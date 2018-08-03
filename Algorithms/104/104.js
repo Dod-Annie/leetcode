ce43ee272f8f0f2a388011abe2f1fbfef12b5ff6
Dod-Annie <936166900@qq.com>
2018-08-03 17:05:16 +0800

    add 104.js

---
+++ Algorithms/104/104.js
@@ -0,0 +1,18 @@
+// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/
+// Author : Dod_Annie
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root === null) return 0
+  return 1 + (Math.max(maxDepth(root.left), maxDepth(root.right)))
+};
